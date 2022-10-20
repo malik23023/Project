4ad7b7dfcbd0ceb75aabe7aea12ab5ffb9351550
Muhammad Abdullah <i201779@nu.edu.pk>
2022-10-20 14:47:29 +0500

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. now making the changes  ashadhjasduasd
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <table>
-          <tr>
-            <td>
-              hiiiiiiiiiiiiiiiiiiiiiiii
-            </td>
-          </tr>
-        </table>
-        <button >
-          hasahsahshas
-        </button>
-      </header>
+    <div className="view">
+    <title>MY BANK</title>
+    <link rel ="stylesheet" href='App.css'/>
+    <nav className='nav-bar'>
+    </nav>
+
+    <h1>WELCOME CASHIER! </h1>
+    <h2>Login to Proceed</h2>
+    
     </div>
+    
   );
 }
+/*
+function App1(){
+  return(
+
+     <p>
+      HIIIIIII YARAAAAAAAAAAAAA 
+     </p>
+     
+  );
+}
+*/
+
+
 
 export default App;
